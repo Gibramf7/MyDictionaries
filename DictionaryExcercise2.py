28ef97f42bb0594fe6a4dbd23f6b443bdb290ebb
Gibram Favela <Gibram_Favela1@baylor.edu>
2021-02-06 18:42:17 -0600

    Tenth commit

--- DictionaryExcercise2.py
+++ DictionaryExcercise2.py
@@ -1,64 +1,46 @@
-# def main():
-# infile = open("WorldSeriesWinners.txt", "r")  # Open the file
-
-# for line in open("WorldSeriesWinners.txt"):
-# line = line.strip()  # But wait, strip the newlines first!
-
-# winners = infile.read().splitlines()  # Read the contents of the file into the list
-
-# Always remember to close the file
-
-# team = input("Enter the name of a team: ")
-
-# counter = 0  # If said team won a game, count how many times
-
-# for winner in winners:  # convert input to lowercase for case-insensitive input
-# if team == winners:
-#   counter = counter + 1
-
-# if counter == 1:  # Finally, print the results
-#    print(
-#        "The", team, "won the world series", counter, "time between 1903 and 2009."
-#    )
-# else:
-#    print(
-#       "The", team, "won the world series", counter, "times between 1903 and 2009."
-#    )
-
-# infile.close()
-
-
-# main()
-
-
 def main():
-    infile = open("WorldSeriesWinners.txt", "r")  # Open the file
+    # Open file
+    infile = open("WorldSeriesWinners.txt", "r")
 
+    # Strip lines
     for line in open("WorldSeriesWinners.txt"):
-        line = line.rstrip("\n")  # But wait, strip the newlines first!
+        line = line.rstrip("\n")
 
-    winners = infile.read().splitlines()  # Read the contents of the file into the list
+    # Split and Read contents of the file
+    winners = infile.read().splitlines()
 
-    infile.close()  # Always remember to close the file
+    # Close file
+    infile.close()
 
-    team = input("Enter the name of a team: ")
+    # User input
+    team_entered = input("Enter name of team: ")
 
-    counter = 0  # If said team won a game, count how many times
+    counter = 0
 
-    for winner in winners:  # convert input to lowercase for case-insensitive input
-        if team.lower() == winner.lower():
+    # Lowercase Conversion
+    for winner in winners:
+        if team_entered.lower() == winner.lower():
             counter = counter + 1
 
-    if counter == 1:  # Finally, print the results
+    # Output
+    if counter == 1:
         print(
-            "The", team, "won the world series", counter, "time between 1903 and 2009."
+            "The",
+            team_entered,
+            "won the world series",
+            counter,
+            "time between 1903 and 2009.",
         )
     elif counter > 1:
         print(
-            "The", team, "won the world series", counter, "times between 1903 and 2009."
+            "The",
+            team_entered,
+            "won the world series",
+            counter,
+            "times between 1903 and 2009.",
         )
     else:
-        print("The", team, "never won the world series.")
+        print("The", team_entered, "never won the world series.")
 
 
 main()
